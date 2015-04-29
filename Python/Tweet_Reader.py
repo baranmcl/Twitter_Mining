c031ad036b9c65f29a8111fa37d940de24cf087d
McLean Baran <baranmcl@gmail.com>
2015-04-29 17:50:03 -0400

    simple test
    added the tasty meat of the code

--- Python/Tweet_Reader.py
+++ Python/Tweet_Reader.py
@@ -1,2 +1,17 @@
 import tweepy
 
+#enter these for the code to work
+consumer_key =
+consumer_secret =
+access_token =
+access_token_secret =
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print tweet.text
